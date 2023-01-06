dfcbddabd2e0088eba2b1123a94da1e0e3bace85
Josh <zylo.codes@gmail.com>
2023-01-05 19:58:13 -0600

    Field validation

--- models/Pets.js
+++ models/Pets.js
@@ -1,9 +1,19 @@
 const { Schema, model } = require('mongoose')
 
 const PetSchema = new Schema({
-  name: String,
-  age: Number,
-  type: String,
+  name: {
+    type: String,
+    required: "A name is required",
+  },
+  age: {
+    type: Number,
+    required: "An age is required",
+    default: 0,
+  },
+  type: {
+    type: String,
+    required: "A animal type is required"
+  },
   // TODO: owner
   // TODO: colors
 })
