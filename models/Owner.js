cef6d4f360e9871fd65bdd4e066bdc9045c70d2a
Josh <zylo.codes@gmail.com>
2023-01-09 19:13:06 -0600

    Virtuals

--- models/Owner.js
+++ models/Owner.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose')
 
 const OwnerSchema = new mongoose.Schema({
-  name: {
+  firstName: {
     type: String,
     required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+}, {
+  toJSON: {
+    virtuals: true,
   }
 })
+
+OwnerSchema.virtual('fullName')
+  .get(function() {
+    return `${this.firstName} ${this.lastName}`
+  })
+  .set(function(value) {
+    const [firstName, lastName] = value.split(' ')
+    this.set({ firstName, lastName })
+  })
 
 const Owner = mongoose.model('Owner', OwnerSchema)
 
