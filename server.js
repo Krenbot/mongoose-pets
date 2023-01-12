2821fc0ce50d4b127e1d7840298325dc2e98899b
Steve Krenn <stevekrenn@gmail.com>
2023-01-11 18:25:00 -0600

    changed hardcoded port to process env port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = require('./routes')
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(routes)
